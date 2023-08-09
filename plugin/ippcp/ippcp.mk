b27419fd24fa0872febcda63dd89e081b4bc3765
nikoPLP <niko@nextgraph.org>
2023-08-09 22:14:08 +0300

    Squashed 'librocksdb-sys/rocksdb/' changes from a397fbd0e..93c3369d3
    93c3369d3 fix tests
    e393b5183 separating binary lib of ippcp by target platform
    
    git-subtree-dir: librocksdb-sys/rocksdb
    git-subtree-split: 93c3369d398392d53802e6b2b4c3c1276c9eb377

--- plugin/ippcp/ippcp.mk
+++ plugin/ippcp/ippcp.mk
@@ -1,4 +1,4 @@
 ippcp_SOURCES = ippcp_provider.cc
 ippcp_HEADERS = ippcp_provider.h
-ippcp_LDFLAGS = -lippcp 
+ippcp_LDFLAGS = -lippcp -Lplugin/ippcp/library/linux/lib
 ippcp_CXXFLAGS = -Iplugin/ippcp/library/include
