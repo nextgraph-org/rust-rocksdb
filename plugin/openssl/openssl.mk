65eeca5272962c3d68df8abf650028826743275b
Niko PLP <niko@nextgraph.org>
2023-08-14 17:09:29 +0300

    Squashed 'librocksdb-sys/rocksdb/' changes from ba14cbef4..4de4c7d0d
    4de4c7d0d removed unnecessary header in openssl plugin
    82db9514a remove typoin makefile
    
    git-subtree-dir: librocksdb-sys/rocksdb
    git-subtree-split: 4de4c7d0d6789817d217331ab9d863c93ffcaedb

--- plugin/openssl/openssl.mk
+++ plugin/openssl/openssl.mk
@@ -1,4 +1,4 @@
 openssl_SOURCES = openssl_provider.cc
 openssl_HEADERS = openssl_provider.h
 openssl_LDFLAGS = -lcrypto
-openssl_CXXFLAGS = -Iplugin/openssl/include -isystemplgun/openssl/include
+openssl_CXXFLAGS = -Iplugin/openssl/include
