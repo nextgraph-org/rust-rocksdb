2026e3002eac5f7389697883b36eb1c4090e8c46
Niko PLP <niko@nextgraph.org>
2023-08-14 18:36:27 +0300

    c++ dyn lib for android

--- librocksdb-sys/build.rs
+++ librocksdb-sys/build.rs
@@ -201,6 +201,7 @@ fn build_rocksdb() {
         config.define("ROCKSDB_PLATFORM_POSIX", None);
         config.define("ROCKSDB_LIB_IO_POSIX", None);
         config.define("OPENSSL_NO_STDIO", None);
+        config.define("ANDROID_STL", "c++_shared");
         config.define("_REENTRANT", None);
         config.flag("-fno-builtin-memcmp");
     } else if target.contains("linux") {
