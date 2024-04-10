e639f75165dc745ed6354b8adbb0a9fe8bf0f6e3
Niko PLP <niko@nextgraph.org>
2024-04-10 19:02:23 +0300

    added rocksdb_transactiondb_flush_cfs_with_status

--- librocksdb-sys/api/c.cc
+++ librocksdb-sys/api/c.cc
@@ -202,6 +202,17 @@ void rocksdb_transactiondb_flush_cf_with_status(
   SaveStatus(statusptr, db->rep->Flush(options->rep, column_family->rep));
 }
 
+void rocksdb_transactiondb_flush_cfs_with_status(
+    rocksdb_transactiondb_t* db, const rocksdb_flushoptions_t* options,
+    rocksdb_column_family_handle_t** column_families, int num_column_families,
+    rocksdb_status_t* statusptr) {
+  vector<ColumnFamilyHandle*> column_family_handles(num_column_families);
+  for (int i = 0; i < num_column_families; i++) {
+    column_family_handles[i] = column_families[i]->rep;
+  }
+  SaveStatus(statusptr, db->rep->Flush(options->rep, column_family_handles));
+}
+
 void rocksdb_transactiondb_compact_range_cf_opt_with_status(
     rocksdb_transactiondb_t* db, rocksdb_column_family_handle_t* column_family,
     rocksdb_compactoptions_t* opt, const char* start_key, size_t start_key_len,
